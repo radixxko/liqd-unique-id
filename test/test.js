b7d30fa8f835bc4cd3e85b069f017c37197809b1
Tomas Korenko <radixxko@gmail.com>
2018-03-26 08:25:04 +0200

    Tests

---
+++ test/test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const generatedIDs = new Set();
+
+const UniqueID = require('../lib/unique_id');
+
+let sessionID = new UniqueID({ unique_interval: 365 * 24 * 3600 });
+
+let start = process.hrtime();
+let ids = 0;
+
+function generate()
+{
+  let id;
+  let elapsed = process.hrtime(start);
+
+  if( elapsed[0] > 5 )
+  {
+    console.log( ids + ' ids' );
+    console.log( ( ids / ( elapsed[0] + elapsed[1] / 1e9 ) ) + ' ids / sec' );
+
+    process.exit();
+  }
+
+  for( let i = 0; i < 256; ++i )
+  {
+    id = sessionID.get();
+
+    ++ids;
+
+    //console.log(id);
+
+
+    if( !generatedIDs.has( id ) )
+    {
+      generatedIDs.add( id );
+    }
+    else
+    {
+      console.error( 'Duplicate', id );
+    }
+  }
+
+  setTimeout( generate, 300 );
+}
+
+generate()
+
+//setInterval( generate, 1000 );
